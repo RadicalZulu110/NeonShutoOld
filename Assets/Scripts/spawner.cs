45b3e9dfe307c45c7503c8246944d633f4df7f95
thomasldw <thomasldw@gmail.com>
2022-03-09 18:18:54 +0000

    Fully randomised spawner

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -6,11 +6,19 @@ public class spawner : MonoBehaviour
  {
      public Transform[] teleport;
      public GameObject[] prefeb;
+      public Transform[] teleport2;
+     public GameObject[] prefeb2;
+float x;
+ float y;
+ float z;
+ Vector3 pos;
      // Use this for initialization
      void Start () 
         {
-         for (int i=0;i<=1;i++){
-             SpawnPrefeb();
+     
+         for (int i=0;i<=30;i++){
+             spawntargetpoint();
+            //  SpawnPrefeb();
          }
      }
  
@@ -20,8 +28,21 @@ public class spawner : MonoBehaviour
      }
      void SpawnPrefeb(){
          int tele_num = Random.Range (0, 0);
-         int prefeb_num = Random.Range (0, 1);
+         int prefeb_num = Random.Range (0, 2);
          Instantiate (prefeb [prefeb_num], teleport [tele_num].position, teleport [tele_num].rotation);
  
+     }
+
+     void spawntargetpoint(){
+         x = Random.Range(-49, 49);
+     z = Random.Range(-49, 49);
+     y = 0;
+     pos = new Vector3(x, y, z);
+     transform.position = pos;
+             int tele_num2 = Random.Range (0, 0);
+         int prefeb_num2 = Random.Range (0, 2);
+         Instantiate (prefeb [prefeb_num2],  pos, Quaternion.identity);
+    Debug.Log(x);
+    Debug.Log(y);
      }
  }
