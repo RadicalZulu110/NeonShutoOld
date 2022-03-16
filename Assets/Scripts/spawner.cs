bc5d277f41e3864341a8b30604c18d4cddb1804e
Michael Selby <mikeselby2@hotmail.co.uk>
2022-03-16 17:10:52 +0000

    UpdateUI_0.1
    Ive redone the UI so its visually more appealing and eaier to navigate and ive added in Tom's spawner and timer scripts

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -1,27 +1,60 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class spawner : MonoBehaviour
  {
-     public Transform[] teleport;
-     public GameObject[] prefeb;
-     // Use this for initialization
-     void Start () 
+    public GameObject Gridobject;
+    public Transform[] teleport;
+    public GameObject[] prefab;
+    public Transform[] teleport2;
+    public GameObject[] prefab2;
+
+    float x;
+    float y;
+    float z;
+    
+    Vector3 pos;  
+
+
+    public GameObject [,] Spawnposition;
+
+    void Start () 
+    {
+        Spawnposition = Gridobject.GetComponent<Grid>().getGrid();
+        
+        for (int i=0;i<=30;i++)
         {
-         for (int i=0;i<=1;i++){
-             SpawnPrefeb();
-         }
-     }
- 
-     // Update is called once per frame
-     void Update () {
- 
-     }
-     void SpawnPrefeb(){
-         int tele_num = Random.Range (0, 0);
-         int prefeb_num = Random.Range (0, 1);
-         Instantiate (prefeb [prefeb_num], teleport [tele_num].position, teleport [tele_num].rotation);
- 
-     }
- }
+            spawntargetpoint();
+            //  SpawnPrefab();
+        }
+    }
+    
+    void SpawnPrefab()
+    {
+        int   Spawnpositionx = Random.Range(0,100);
+        int   Spawnpositiony = Random.Range(0,100);
+
+        //sets how many object types can be added currently only 2 can be added
+        int prefab_num = Random.Range(0,2);
+        Instantiate (prefab [prefab_num], Spawnposition[Spawnpositionx,Spawnpositiony].transform); 
+    }
+    
+    void spawntargetpoint()
+    {
+        x = Random.Range(-49, 49);
+        z = Random.Range(-49, 49);
+        y = 0;
+
+        pos = new Vector3(x, y, z);
+        transform.position = pos;
+
+        int tele_num2 = Random.Range (0, 0);
+        int prefab_num2 = Random.Range (0, 2);
+
+        Instantiate (prefab [prefab_num2],  pos, Quaternion.identity);
+        Debug.Log(x);
+        Debug.Log(y);
+    }
+}
---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    public int seconds;
+    public int min;
+    public int hour;
+    public int day;
+    public int week;
+    public int month;
+    public float timeBtwIncrease;
+    private float nextIncreaseTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     private void Update()
+    {
+        if (Time.time > nextIncreaseTime)
+        {
+            nextIncreaseTime = Time.time + timeBtwIncrease;
+            Debug.Log(seconds + "seconds");
+            // Debug.Log(seconds + " seconds");
+    if (seconds <= 59)
+    {
+        seconds++;
+    }
+
+
+     else if (seconds >= 59)
+        {
+           seconds = 0;
+          min = min+1;
+          Debug.Log(min);
+         }
+        
+
+    
+  // if (min <= 60 && seconds >= 59)
+  //   {
+  //       min++;
+  //   }
+
+  //    if (min >= 60)
+  //       {
+  //         int min = 0;
+  //         hour++;
+  //         Debug.Log(hour);
+  //       }
+
+  //       }
+
+  //         if (hour <= 24 && day <= 7)
+  //   {
+  //       day++;
+  //   }
+
+  //    if (day >= 7)
+  //       {
+  //         int day = 0;
+  //         week++;
+  //         Debug.Log(week);
+  //       }
+
+  //            if (week <= 4 && month >= 12)
+  //   {
+  //       week++;
+        
+  //   }
+
+  //    if ( week>= 7)
+  //       {
+  //         int week = 0;
+  //         month++;
+  //         Debug.Log(month);
+  //       }
+    }
+    }
+}
+
